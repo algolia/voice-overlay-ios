04f4eb8fd4f974b4a221724de6ddda851ef04ba0
Robert Mogos <robert.mogos@algolia.com>
2018-10-31 11:24:13 +0100

    Add comments for the Recordable class

--- VoiceOverlay/Protocols/Recordable.swift
+++ VoiceOverlay/Protocols/Recordable.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
+/// Protocol that allows to inject any STT into the voice overlay
 public protocol Recordable {
+  
+  // Starting the recording and processing the STT
   func startRecording(textHandler: @escaping SpeechTextHandler, errorHandler: @escaping SpeechErrorHandler)
   
+  // Stop the recording
   func stopRecording()
   
+  // Get the state of the recording
   func isRecording() -> Bool
   
+  // Request system authorisation for STT and Voice capabilies
   func requestAuthorization(_ statusHandler: @escaping (Bool) -> Void)
 }
 
